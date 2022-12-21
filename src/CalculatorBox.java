e4c81818c52ae0fb6d6638728dc2e2bc1fc78d70
rushLovenshier <ruwandi.2001.18@gmail.com>
2022-12-21 13:34:32 +0530

    Initialized components

--- src/CalculatorBox.java
+++ src/CalculatorBox.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author ruwandigeekiyanage on 2022-12-21.
 
  */
 public class CalculatorBox extends JFrame {
-JButton btnAdd, btnSubtract, btnDivide, btnMultiple, btnClear, btnDot;
+JButton btnAdd, btnSubtract, btnDivide, btnMultiply, btnClear, btnDelete, btnDot, btnEquals;
 JButton numBtn[];
 JTextField output;
 String previous, current, operator;
@@ -14,6 +15,27 @@ CalculatorBox(){
     super("Box Calculator");
 
     JPanel mainPanel = new JPanel();
+
+    //Create sub panels inside main panel
+    JPanel row1 = new JPanel();
+    JPanel row2 = new JPanel();
+    JPanel row3 = new JPanel();
+    JPanel row4 = new JPanel();
+    JPanel row5 = new JPanel();
+
+    //Initialize components
+
+    output = new JTextField(16);
+    btnAdd = new JButton("+");
+    btnSubtract = new JButton("-");
+    btnDivide = new JButton("÷");
+    btnMultiply = new JButton("*");
+
+    btnDot = new JButton(".");
+    btnClear = new JButton("clear");
+    btnDelete = new JButton("D");
+    btnEquals = new JButton("=");
+
 
 }
 }
--- src/CalculatorDemo.java
+++ src/CalculatorDemo.java
@@ -2,6 +2,7 @@
  * @author ruwandigeekiyanage on 2022-12-21.
 
  */
+
 public class CalculatorDemo {
     public static void Main(String[] args){
         new CalculatorBox();
