7bf1a208da7600e1c8cc2fb1863fe3bf4f0cf87d
rushLovenshier <ruwandi.2001.18@gmail.com>
2022-12-21 14:17:24 +0530

    Make frame visible

--- src/CalculatorBox.java
+++ src/CalculatorBox.java
@@ -61,5 +61,43 @@ CalculatorBox(){
     output.setDisabledTextColor(new Color(0, 10, 0));
     output.setMargin(new Insets(0,5,0,0));
     output.setText("0");
-}
+
+    //Set the layout of each row in the pane
+    row1.setLayout(new BoxLayout(row1,BoxLayout.LINE_AXIS));
+    row2.setLayout(new BoxLayout(row2,BoxLayout.LINE_AXIS));
+    row3.setLayout(new BoxLayout(row3,BoxLayout.LINE_AXIS));
+    row4.setLayout(new BoxLayout(row4, BoxLayout.LINE_AXIS));
+    row5.setLayout(new BoxLayout(row5, BoxLayout.LINE_AXIS));
+
+    //Add components to each of the row
+    row1.add(Box.createHorizontalGlue());
+    row1.add(btnClear);
+    row1.add(btnDelete);
+
+    row2.add(numBtn[7]); row2.add(numBtn[8]); row2.add(numBtn[9]);
+    row2.add(btnMultiply);
+
+    row3.add(numBtn[4]); row3.add(numBtn[5]); row3.add(numBtn[6]);
+    row3.add(btnAdd);
+
+    row4.add(numBtn[1]); row4.add(numBtn[2]); row4.add(numBtn[3]);
+    row4.add(btnSubtract);
+
+    row5.add(btnDot); row5.add(numBtn[0]); row5.add(btnEquals);
+    row5.add(btnDivide);
+
+    //Add all rows to main panel
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+    mainPanel.add(output);
+    mainPanel.add(row1);
+    mainPanel.add(row2);
+    mainPanel.add(row3);
+    mainPanel.add(row4);
+    mainPanel.add(row5);
+
+    this.add(mainPanel);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    this.setSize(205, 280);
+    }
 }
